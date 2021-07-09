bd1487eae687dd91c6ddd6e9c034736f35094e9d
borzas89 <borza.zsolt89@gmail.com>
2021-07-09 23:08:57 +0200

    adding navigation

---
+++ app/src/main/java/example/com/totalnfl/navigator/AppNavigator.kt
@@ -0,0 +1,7 @@
+package example.com.totalnfl.navigator
+
+interface AppNavigator {
+
+    fun navigateToDetail(id: Long)
+    fun popBackStack()
+}
---
+++ app/src/main/java/example/com/totalnfl/navigator/AppNavigatorImpl.kt
@@ -0,0 +1,24 @@
+package example.com.totalnfl.navigator
+
+import android.app.Activity
+import androidx.navigation.Navigation
+import example.com.totalnfl.R
+import example.com.totalnfl.ui.list.ListFragmentDirections
+import javax.inject.Inject
+
+class AppNavigatorImpl @Inject constructor(
+    private val activity: Activity
+) : AppNavigator {
+
+    private val navController by lazy {
+        Navigation.findNavController(activity, R.id.nav_host_fragment)
+    }
+
+    override fun navigateToDetail(id: Long) {
+        navController.navigate(ListFragmentDirections.actionListFragmentToDetailFragment(id))
+    }
+
+    override fun popBackStack() {
+        navController.popBackStack()
+    }
+}
