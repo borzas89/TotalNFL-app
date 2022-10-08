634aa3c5ef7daa9bef8b6420a448ebde16dafa52
Borza Zsolt <borza.zsolt89@gmail.com>
2022-10-08 22:03:17 +0200

    adding base arch related stuff

---
+++ app/src/main/java/example/com/totalnfl/arch/BaseViewHolder.kt
@@ -0,0 +1,8 @@
+package example.com.totalnfl.arch
+
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+
+class BaseViewHolder<BINDING : ViewDataBinding>(val binder: BINDING) :
+    RecyclerView.ViewHolder(binder.root) {
+}
